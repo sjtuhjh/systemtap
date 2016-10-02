5efde8b5c51d2f459f9fea4eb432f2c39c4d5b74
root <root@huangjinhua>
2016-10-02 22:26:16 +0800

    update setup.sh for other platforms

--- setup.sh
+++ setup.sh
@@ -5,13 +5,14 @@ if [ "$(whoami)" == "root" ] ; then
     SUDO_PREFIX=""
 fi
 
+INSTALL_CMD="yum"
 KERNEL_REL="$(uname -r)"
 
 if [ "$(which apt-get)" ] ; then
-    ${SUDO_PREFIX} apt-get install -yq linux-source
-    ${SUDO_PREFIX} apt-get install -yq linux-image-${KERNEL_REL}-dbgsym
-    ${SUDO_PREFIX} apt-get install -yq systemtap
+    INSTALL_CMD="apt-get"
 fi
 
-
+${SUDO_PREFIX} ${INSTALL_CMD} install -yq linux-source
+${SUDO_PREFIX} ${INSTALL_CMD} install -yq linux-image-${KERNEL_REL}-dbgsym
+${SUDO_PREFIX} ${INSTALL_CMD} install -yq systemtap
 
